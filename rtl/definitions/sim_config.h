847017cf4d41e2fd34c00ca5337edf46941236db
pobu <huangbowen@ict.ac.cn>
2018-07-03 20:37:55 +0800

    fix sim config ignore

--- rtl/definitions/sim_config.h
+++
@@ -1,8 +0,0 @@
-`ifndef SIMULATION
-    `define SIMULATION
-    `timescale 100ps/100ps
-    `define FULL_CYCLE_DELAY 50
-    `define HALF_CYCLE_DELAY 25
-    `define MEM_IMAGE_DIR "/home/pobu/codes/pumpkin/verify/unit_test/mem_image"
-`else
-`endif
