20fdd20a46c7ac9dd9d144d7cca4a598480419a3
cysoho2 <406641909@qq.com>
2018-07-03 01:24:19 -0700

    save

--- rtl/definitions/sim_config.h
+++ rtl/definitions/sim_config.h
@@ -3,7 +3,7 @@
     `timescale 100ps/100ps
     `define FULL_CYCLE_DELAY 100
     `define HALF_CYCLE_DELAY 50
-    `define MEM_IMAGE_DIR "/Users/pobu/Documents/Codes/pumpkin/verify/unit_test/mem_image"
+    `define MEM_IMAGE_DIR "/home/debug/Workspace/Git3/pumpkin/verify/unit_test/mem_image"
     `define DUMP_FILENAME "sim_waves.fst"
 `else
 `endif
