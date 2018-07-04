a7495606b5f2b92833c5a43a899257d7f1925aa3
pobu <huangbowen@ict.ac.cn>
2018-07-05 06:51:06 +0800

    removing replacing policy

--- rtl/unified_cache/replacing_policy.v
+++ rtl/unified_cache/replacing_policy.v
@@ -11,43 +11,7 @@ module replacing_policy
     output  [NUM_WAY - 1 : 0] replaced_way_out
 );
 
-wire [NUM_WAY - 1 : 0] first_valid_way;
-wire                   valid_way_exist;
+// insert your replacing policy here
 
-wire [NUM_WAY - 1 : 0] first_unused_way;
-
-find_first_one_index
-#(
-    .VECTOR_LENGTH(NUM_WAY)
-)
-find_first_valid
-(
-    .vector_in              (NUM_WAY),
-    .first_one_index_out    (first_valid_way),
-    .one_is_found_out       (valid_way_exist)
-);
-
-find_first_one_index
-#(
-    .VECTOR_LENGTH(NUM_WAY)
-)
-find_first_unused
-(
-    .vector_in              (NUM_WAY),
-    .first_one_index_out    (first_unused_way),
-    .one_is_found_out       ()
-);
-
-wire [NUM_WAY - 1 : 0] target_index = valid_way_exist ? first_valid_way : first_unused_way;
-
-generate
-genvar loop_index;
-
-    for(loop_index = 0; loop_index < NUM_WAY; loop_index = loop_index + 1)
-    begin
-        assign replaced_way_out[loop_index] = loop_index == target_index ? 1'b1 : 1'b0; 
-    end
-
-endgenerate
 
 endmodule
