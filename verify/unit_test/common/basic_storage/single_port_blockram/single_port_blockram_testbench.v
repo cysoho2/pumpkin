a4b9c9332bc5886425114b92b2898871d8446680
cysoho2 <406641909@qq.com>
2018-07-04 05:18:47 -0700

    add initial delay

--- verify/unit_test/common/basic_storage/single_port_blockram/single_port_blockram_testbench.v
+++ verify/unit_test/common/basic_storage/single_port_blockram/single_port_blockram_testbench.v
@@ -49,67 +49,67 @@ begin
     
     test_latency                                = 1;
 
-    #(`FULL_CYCLE_DELAY * 5);
+    #(`FULL_CYCLE_DELAY * 25 + `HALF_CYCLE_DELAY);
 
     $display("[info-testbench] %m testbench reset completed\n");
 
     // case 0
-    test_input_1                                            = { {(SINGLE_ENTRY_SIZE_IN_BITS/2){1'b1}}, {(SINGLE_ENTRY_SIZE_IN_BITS/2){1'b0}} };
+    test_input_1                                                                = { {(SINGLE_ENTRY_SIZE_IN_BITS/2){1'b1}}, {(SINGLE_ENTRY_SIZE_IN_BITS/2){1'b0}} };
     
-    access_set_addr_in                                      = NUM_SET - 1;
-    write_entry_in                                          = test_input_1;
+    access_set_addr_in                                                          = NUM_SET - 1;
+    write_entry_in                                                              = test_input_1;
 
-    #(`FULL_CYCLE_DELAY) write_en_in                        = {(WRITE_MASK_LEN){1'b1}};
+    #(`FULL_CYCLE_DELAY) write_en_in                                            = {(WRITE_MASK_LEN){1'b1}};
 
     // latency
-    #(`FULL_CYCLE_DELAY * test_latency) test_result_1       = read_entry_out;
-    #(`FULL_CYCLE_DELAY * test_latency) write_en_in         = 0;
+    #(`FULL_CYCLE_DELAY * test_latency + `HALF_CYCLE_DELAY) test_result_1       = read_entry_out;
+    #(`FULL_CYCLE_DELAY * test_latency) write_en_in                             = 0;
     
-    #(`FULL_CYCLE_DELAY) test_judge                         = (test_result_1 === test_input_1) && (test_result_1 !== {(SINGLE_ENTRY_SIZE_IN_BITS){1'bx}});
+    #(`FULL_CYCLE_DELAY + `HALF_CYCLE_DELAY) test_judge                         = (test_result_1 === test_input_1) && (test_result_1 !== {(SINGLE_ENTRY_SIZE_IN_BITS){1'bx}});
 
 
     $display("[info-testbench] test case %d %40s : \t%s", test_case_num, "basic write-read access", test_judge ? "passed" : "failed");
 
 
     // case 1
-    #(`FULL_CYCLE_DELAY) test_case_num                      = test_case_num + 1;
-    test_result_1                                           = { {(SINGLE_ENTRY_SIZE_IN_BITS/2){1'b1}}, {(SINGLE_ENTRY_SIZE_IN_BITS/2){1'b0}} };
-    test_input_1                                            = { {(SINGLE_ENTRY_SIZE_IN_BITS/2){1'b0}}, {(SINGLE_ENTRY_SIZE_IN_BITS/2){1'b1}} };
+    #(`FULL_CYCLE_DELAY) test_case_num                                          = test_case_num + 1;
+    test_result_1                                                               = { {(SINGLE_ENTRY_SIZE_IN_BITS/2){1'b1}}, {(SINGLE_ENTRY_SIZE_IN_BITS/2){1'b0}} };
+    test_input_1                                                                = { {(SINGLE_ENTRY_SIZE_IN_BITS/2){1'b0}}, {(SINGLE_ENTRY_SIZE_IN_BITS/2){1'b1}} };
     
-    access_set_addr_in                                      = NUM_SET - 1;
-    write_entry_in                                          = test_input_1;
+    access_set_addr_in                                                          = NUM_SET - 1;
+    write_entry_in                                                              = test_input_1;
 
-    #(`FULL_CYCLE_DELAY) write_en_in                        = 0;
+    #(`FULL_CYCLE_DELAY) write_en_in                                            = 0;
 
-    #(`FULL_CYCLE_DELAY * test_latency) test_result_2       = read_entry_out; 
-    #(`FULL_CYCLE_DELAY * test_latency) write_en_in         = 0;
+    #(`FULL_CYCLE_DELAY * test_latency  + `HALF_CYCLE_DELAY) test_result_2      = read_entry_out; 
+    #(`FULL_CYCLE_DELAY * test_latency) write_en_in                             = 0;
 
-    #(`FULL_CYCLE_DELAY) test_judge                         = (test_result_2 === test_result_1) && (test_result_2 !== {(SINGLE_ENTRY_SIZE_IN_BITS){1'bx}});
+    #(`FULL_CYCLE_DELAY + `HALF_CYCLE_DELAY) test_judge                         = (test_result_2 === test_result_1) && (test_result_2 !== {(SINGLE_ENTRY_SIZE_IN_BITS){1'bx}});
 
     $display("[info-testbench] test case %d %40s : \t%s", test_case_num, "write enable verify", test_judge ? "passed" : "failed");
 
 
     // case 2
-    #(`HALF_CYCLE_DELAY) test_case_num                      = test_case_num + 1;
+    #(`FULL_CYCLE_DELAY) test_case_num                                          = test_case_num + 1;
 
-    write_entry_in                                          = 0;
-    write_en_in                                             = {(WRITE_MASK_LEN){1'b1}};
+    write_entry_in                                                              = 0;
+    write_en_in                                                                 = {(WRITE_MASK_LEN){1'b1}};
 
-    access_set_addr_in                                      = NUM_SET - 2;
+    access_set_addr_in                                                          = NUM_SET - 2;
 
-    #(`FULL_CYCLE_DELAY * test_latency * 2) write_en_in     = 0;
+    #(`FULL_CYCLE_DELAY * test_latency * 2) write_en_in                         = 0;
 
-    test_input_1                                            = {(SINGLE_ENTRY_SIZE_IN_BITS){1'b1}};
-    test_result_1                                           = {{(SINGLE_ENTRY_SIZE_IN_BITS / 4){1'b1}}, {(SINGLE_ENTRY_SIZE_IN_BITS / 4){1'b0}}, {(SINGLE_ENTRY_SIZE_IN_BITS / 4){1'b1}},{(SINGLE_ENTRY_SIZE_IN_BITS / 4){1'b0}}};
+    test_input_1                                                                = {(SINGLE_ENTRY_SIZE_IN_BITS){1'b1}};
+    test_result_1                                                               = {{(SINGLE_ENTRY_SIZE_IN_BITS / 4){1'b1}}, {(SINGLE_ENTRY_SIZE_IN_BITS / 4){1'b0}}, {(SINGLE_ENTRY_SIZE_IN_BITS / 4){1'b1}},{(SINGLE_ENTRY_SIZE_IN_BITS / 4){1'b0}}};
 
-    write_entry_in                                          = test_input_1;
+    write_entry_in                                                              = test_input_1;
 
-    #(`FULL_CYCLE_DELAY) write_en_in                        = {{(WRITE_MASK_LEN / 4){1'b1}}, {(WRITE_MASK_LEN / 4){1'b0}}, {(WRITE_MASK_LEN / 4){1'b1}}, {(WRITE_MASK_LEN / 4){1'b0}}};
+    #(`FULL_CYCLE_DELAY) write_en_in                                            = {{(WRITE_MASK_LEN / 4){1'b1}}, {(WRITE_MASK_LEN / 4){1'b0}}, {(WRITE_MASK_LEN / 4){1'b1}}, {(WRITE_MASK_LEN / 4){1'b0}}};
 
-    #(`FULL_CYCLE_DELAY * test_latency) test_result_2       = read_entry_out;    
-    #(`FULL_CYCLE_DELAY * test_latency) write_en_in         = 0;
+    #(`FULL_CYCLE_DELAY * test_latency + `HALF_CYCLE_DELAY) test_result_2       = read_entry_out;    
+    #(`FULL_CYCLE_DELAY * test_latency) write_en_in                             = 0;
 
-    #(`FULL_CYCLE_DELAY) test_judge                         = (test_result_2 === test_result_1) && (test_result_2 !== {(SINGLE_ENTRY_SIZE_IN_BITS){1'bx}});
+    #(`FULL_CYCLE_DELAY) test_judge                                             = (test_result_2 === test_result_1) && (test_result_2 !== {(SINGLE_ENTRY_SIZE_IN_BITS){1'bx}});
 
     $display("[info-testbench] test case %d %40s : \t%s", test_case_num, "write mask enable verify", test_judge ? "passed" : "failed");
 
