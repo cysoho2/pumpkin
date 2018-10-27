bda64e41c77ae2781482e1c504dbfa9bc3887979
cysoho2 <406641909@qq.com>
2018-10-27 10:52:44 +0800

    save

--- rtl/common/basic_logic/adder_tree.v
+++ rtl/common/basic_logic/adder_tree.v
@@ -1,9 +1,9 @@
 module adder_tree
 #(
-    parameter NUM_ADDER                                                     = 1,
-    parameter OPERAND_RGHIT_SHIFT_IN_BITS                                   = 0,
-    parameter OPERAND_WIDTH_IN_BITS                                         = 64,
-    parameter RESULT_WIDTH_IN_BITS                                          = 64
+    parameter NUM_ADDER                                                     = 2,
+    parameter OPERAND_RGHIT_SHIFT_IN_BITS                                   = 32,
+    parameter OPERAND_WIDTH_IN_BITS                                         = 96,
+    parameter RESULT_WIDTH_IN_BITS                                          = 128
 )
 (
     input                                                                   reset_in,
@@ -12,12 +12,13 @@ module adder_tree
     input          [OPERAND_WIDTH_IN_BITS * NUM_ADDER - 1 : 0]              operand_package_1_in,
     input          [OPERAND_WIDTH_IN_BITS * NUM_ADDER - 1 : 0]              operand_package_2_in,
     
-    output reg     [RESULT_WIDTH_IN_BITS * NUM_ADDER - 1 : 0]               result_package_out                            
+    output reg     [RESULT_WIDTH_IN_BITS * NUM_ADDER - 1 : 0]               result_package_out                           
 );
 
 
 parameter [31 : 0] OPERAND_PACKAGE_SIZE                                     = OPERAND_WIDTH_IN_BITS * NUM_ADDER;
 parameter [31 : 0] RESULT_PACKAGE_SIZE                                      = RESULT_WIDTH_IN_BITS * NUM_ADDER;
+
 
 genvar gen;
 generate
@@ -25,28 +26,30 @@ generate
     for (gen = 0; gen < NUM_ADDER; gen = gen + 1)
     begin
     
-        wire [RESULT_WIDTH_IN_BITS - 1 : 0]     gen_operand_1;
-        wire [RESULT_WIDTH_IN_BITS - 1 : 0]     gen_operand_2;
-        
-        wire [RESULT_WIDTH_IN_BITS - 1 : 0]     gen_result; 
+        parameter OPERAND_PACKAGE_POS_HI                                    = OPERAND_PACKAGE_SIZE - gen * OPERAND_WIDTH_IN_BITS - 1;
+        parameter OPERAND_PACKAGE_POS_LO                                    = OPERAND_PACKAGE_SIZE - (gen+ 1) * OPERAND_WIDTH_IN_BITS;
+        parameter RESULT_PACKAGE_POS_HI                                     = RESULT_PACKAGE_SIZE - gen * RESULT_WIDTH_IN_BITS - 1;
+        parameter RESULT_PACKAGE_POS_LO                                     = RESULT_PACKAGE_SIZE - (gen + 1) * RESULT_WIDTH_IN_BITS;
     
-        assign gen_operand_1 = 
-            {{operand_package_1_in[OPERAND_PACKAGE_SIZE - gen * OPERAND_WIDTH_IN_BITS - 1 : OPERAND_PACKAGE_SIZE - (gen+ 1) * OPERAND_WIDTH_IN_BITS]}, {(OPERAND_RGHIT_SHIFT_IN_BITS){1'b0}}};
-        assign gen_operand_2 =
-            {{(RESULT_WIDTH_IN_BITS - OPERAND_WIDTH_IN_BITS){1'b0}}, {operand_package_2_in[OPERAND_PACKAGE_SIZE - gen * OPERAND_WIDTH_IN_BITS - 1 : OPERAND_PACKAGE_SIZE - (gen + 1) * OPERAND_WIDTH_IN_BITS]}};
+        wire [RESULT_WIDTH_IN_BITS - 1 : 0]                                 gen_operand_1;
+        wire [RESULT_WIDTH_IN_BITS - 1 : 0]                                 gen_operand_2;
         
-        assign gen_result = result_package_out[RESULT_PACKAGE_SIZE - gen * RESULT_WIDTH_IN_BITS - 1 : RESULT_PACKAGE_SIZE - (gen + 1) * RESULT_WIDTH_IN_BITS];
+        wire [RESULT_WIDTH_IN_BITS - 1 : 0]                                 gen_result; 
+    
+        assign gen_operand_1                                                = {{operand_package_1_in[OPERAND_PACKAGE_POS_HI : OPERAND_PACKAGE_POS_LO]}, {(OPERAND_RGHIT_SHIFT_IN_BITS){1'b0}}};
+        assign gen_operand_2                                                = {{(OPERAND_RGHIT_SHIFT_IN_BITS){1'b0}}, {operand_package_2_in[OPERAND_PACKAGE_POS_HI : OPERAND_PACKAGE_POS_LO]}};
+        
+        assign gen_result                                                   = result_package_out[RESULT_PACKAGE_POS_HI : RESULT_PACKAGE_POS_LO];
         
         always@(posedge clk_in, posedge reset_in)
         begin
             if (reset_in)
             begin
-                result_package_out <= {(RESULT_PACKAGE_SIZE){1'b0}};
+                result_package_out[RESULT_PACKAGE_POS_HI : RESULT_PACKAGE_POS_LO]   <= {(RESULT_WIDTH_IN_BITS){1'b0}};
             end
             else
-            begin
-                result_package_out[RESULT_PACKAGE_SIZE - gen * RESULT_WIDTH_IN_BITS - 1 : RESULT_PACKAGE_SIZE - (gen + 1) * RESULT_WIDTH_IN_BITS] =
-                     (OPERAND_WIDTH_IN_BITS < RESULT_WIDTH_IN_BITS) ? {{(RESULT_WIDTH_IN_BITS - OPERAND_WIDTH_IN_BITS){1'b0}}, gen_operand_1 + gen_operand_2} : gen_operand_1 + gen_operand_2;   
+            begin 
+               result_package_out[RESULT_PACKAGE_POS_HI : RESULT_PACKAGE_POS_LO]    = gen_operand_1 + gen_operand_2; 
             end
         end
        
--- verify/unit_test/common/basic_logic/adder_tree/adder_tree_testbench.v
+++ verify/unit_test/common/basic_logic/adder_tree/adder_tree_testbench.v
@@ -5,9 +5,9 @@ module adder_tree_testbench();
 
 reg                     clk_in;
 reg                     reset_in;
-reg [2 * 96 - 1 : 0]    operand_package_1_in;
-reg [2 * 96 - 1 : 0]    operand_package_2_in;
-wire [2 * 128 - 1 : 0]  result_package_out;
+reg [2 * 80 - 1 : 0]    operand_package_1_in;
+reg [2 * 80 - 1 : 0]    operand_package_2_in;
+wire [2 * 96 - 1 : 0]  result_package_out;
 
 
 
@@ -23,17 +23,18 @@ begin
     
                                     $display("\n[info-testbench] simulation for %m begins now");
                                     
-                                    clk_in                  = 1'b0;
+                                    clk_in                  = 1'b1;
                                     reset_in                = 1'b1;
-                                    
-     #(`FULL_CYCLE_DELAY * 2)       operand_package_1_in    = {{(96){1'b0}}, {(96){1'b1}}};
-                                    operand_package_2_in    = {{(96){1'b1}}, {(96){1'b0}}};
+     #(`FULL_CYCLE_DELAY * 50)                               
+     #(`FULL_CYCLE_DELAY * 1.5)       operand_package_1_in    = {{(80){1'b0}}, {(80){1'b1}}};
+                                    operand_package_2_in    = {{(80){1'b1}}, {(80){1'b0}}};
      
-     #(`FULL_CYCLE_DELAY * 2)       reset_in                = 1'b0;
-    
+     #(`FULL_CYCLE_DELAY * 1)       reset_in                = 1'b0;
+     #(`FULL_CYCLE_DELAY * 2)          operand_package_1_in    <= 0;
+                                    operand_package_2_in    <= 0;
     
 
-     #(`FULL_CYCLE_DELAY * 3000)    $display("\n[info-testbench] simulation for %m comes to the end\n");
+     #(`FULL_CYCLE_DELAY * 30)    $display("\n[info-testbench] simulation for %m comes to the end\n");
                                     $finish;
 
 end
@@ -41,9 +42,9 @@ end
 adder_tree
 #(
     .NUM_ADDER(2),
-    .OPERAND_RGHIT_SHIFT_IN_BITS(32),
-    .OPERAND_WIDTH_IN_BITS(96),
-    .RESULT_WIDTH_IN_BITS(128)
+    .OPERAND_RGHIT_SHIFT_IN_BITS(16),
+    .OPERAND_WIDTH_IN_BITS(80),
+    .RESULT_WIDTH_IN_BITS(96)
 )
 adder_tree
 (
