1f308f65d7bfca32ed6f0c0f612ce2bb1210bc36
cysoho2 <406641909@qq.com>
2018-11-08 21:15:26 +0800

    improve float_point_adder.v

--- rtl/common/basic_logic/float_point_adder.v
+++ rtl/common/basic_logic/float_point_adder.v
@@ -1,44 +1,4 @@
-;`include parameters.h
-
-
-
-module float_point_adder
-
-#(
-
-	    parameter OPERAND_EXPONENT_WIDTH_IN_BITS = `DOUBLE_POINT_NUMBER_EXPONENT_WIDTH_IN_BITS,
-
-		    parameter OPERAND_FRACTION_WIDTH_IN_BITS = `DOUBLE_POINT_NUMBER_FRACTION_WIDTH_IN_BITS
-
-			)
-
-			(
-
-				    input                                                   reset_in,
-
-					    input                                                   clk_in,
-
-
-
-						    input                                                   operantion_mode_in,
-
-
-
-							    input                                                   operand_0_valid_in,
-
-								    input                                                   operand_0_sign_in,
-
-									    input       [(OPERAND_EXPONENT_WIDTH_IN_BITS - 1):0]    operand_0_exponent_in,
-
-										    input       [(OPERAND_FRACTION_WIDTH_IN_BITS - 1):0]    operand_0_fraction_in,
-
-
-
-											    input                                                   operand_1_valid_in,
-
-												    input                                                   operand_1_sign_in,
-
-													    input       [(OPERAND_EXPONENT_WIDTH_IN_BITS - 1):0]    ;`include parameters.h
+`include parameters.h
 
 module float_point_adder
 #(
@@ -71,20 +31,23 @@ module float_point_adder
     input                                                   issue_ack_in,
 );
 
+parameter ROUND_TYPE = "CHOP",
+
+
 parameter ADD_OPERANTION = 0;
 parameter SUB_OPERANTION = 1;
 
-parameter STATE_WAIT_INPUT  = 0;
+parameter STATE_WAIT_RESET  = 0;
 parameter STATE_PRE_SHIFT   = 1;
 parameter STATE_COMPUTE     = 2;
 parameter STATE_POST_SHIFT  = 3;
 
+reg                                                   operantion_mode_buffer,
+
 reg                                                   operand_0_sign_buffer;
-reg       [(OPERAND_EXPONENT_WIDTH_IN_BITS - 1):0]    operand_0_exponent_buffer;
 reg       [(OPERAND_FRACTION_WIDTH_IN_BITS - 1):0]    operand_0_fraction_buffer;
 
 reg                                                   operand_1_sign_buffer;
-reg       [(OPERAND_EXPONENT_WIDTH_IN_BITS - 1):0]    operand_1_exponent_buffer;
 reg       [(OPERAND_FRACTION_WIDTH_IN_BITS - 1):0]    operand_1_fraction_buffer;
 
 reg       [1:0] ctrl_state;
@@ -93,24 +56,24 @@ reg       [(OPERAND_EXPONENT_WIDTH_IN_BITS - 1):0]    baseline_exponent_buffer;
 reg       [(OPERAND_EXPONENT_WIDTH_IN_BITS - 1):0]    fraction_pre_shift_len;
 
 //control sign
-wire      input_enable;
+reg       operand_buffer_write_enable;
+reg       baseline_exponent_write_enable;
+reg       fraction_pre_shift_len_write_enable;
 
-wire      operand_buffer_write_enable;
-wire      baseline_exponent_write_enable;
-wire      fraction_pre_shift_len_write_enable;
+reg       rounded_product_buffer_write_enable;
 
-wire      rounded_product_buffer_write_enable;
+reg       sign_out_write_enable;
+reg       exponent_out_write_enable;
+reg       fraction_out_write_enable;
+reg       clear_output_enable;
 
-wire      sign_out_write_enable;
-wire      exponent_out_write_enable;
-wire      fraction_out_write_enable;
-wire      clear_output_enable;
-
-
+wire      valid_input_flag;
 
 wire                                                  operand_0_exponent_is_larger;
 wire      [(OPERAND_EXPONENT_WIDTH_IN_BITS - 1):0]    difference_of_exponents;
 wire      [(OPERAND_EXPONENT_WIDTH_IN_BITS - 1):0]    data_to_baseline_exponent_buffer;
+
+assign    valid_input_flag = operand_0_valid_in & operand_1_valid_in;
 
 assign    difference_of_exponents = operand_0_exponent_in - operand_1_exponent_in;
 assign    operand_0_exponent_is_larger = ~ difference_of_exponents[(OPERAND_EXPONENT_WIDTH_IN_BITS - 1)];
@@ -122,11 +85,9 @@ begin
     if (reset_in)
     begin
         operand_0_sign_buffer <= 1'b0;
-        operand_0_exponent_buffer <= {(OPERAND_FRACTION_WIDTH_IN_BITS){1'b0}};
         operand_0_fraction_buffer <= {(OPERAND_FRACTION_WIDTH_IN_BITS){1'b0}};
 
         operand_1_sign_buffer <= 1'b0;
-        operand_1_exponent_buffer <= {(OPERAND_FRACTION_WIDTH_IN_BITS){1'b0}};
         operand_1_fraction_buffer <= {(OPERAND_FRACTION_WIDTH_IN_BITS){1'b0}};
     end
     else
@@ -139,7 +100,7 @@ begin
         begin
             if (input_enable)
             begin
-                if (operand_0_valid_in & operand_1_valid_in)
+                if (valid_input_flag)
                 begin
                     issue_ack_out <= 1'b1;
 
@@ -176,57 +137,125 @@ always @(posedge clk_in)
 begin
     if (reset_in)
     begin
-        ctrl_state <= 0;
+        ctrl_state <= STATE_RESET;
     end
     else
     begin
         case (ctrl_state)
-            STATE_WAIT_INPUT: begin
-                if (issue_ack_out)
-                begin
-                    ctrl_state <= ctrl_state + 1'b1;
-                end
+            STATE_RESET: begin
+                ctrl_state <= STATE_PRE_SHIFT;
             end
 
             STATE_PRE_SHIFT: begin
-
-            end
-
-            STATE_COMPUTE: begin
-
-            end
-
-            STATE_POST_SHIFT: begin
-                if (issue_ack_in)
+                if (valid_input_flag)
                 begin
-                    ctrl_state <= 0;
+                    ctrl_state <= STATE_COMPUTE;
+                end
+                else
+                begin
+                    ctrl_state <= ctrl_state;
                 end
             end
 
+            STATE_COMPUTE: begin
+                ctrl_state <= STATE_POST_SHIFT;
+            end
+
+            STATE_POST_SHIFT: begin
+                ctrl_state <= STATE_PRE_SHIFT
+            end
+
             default: begin
-                ctrl_state <= 0;
+                ctrl_state <= STATE_RESET;
             end
 
         endcase
     end
 end
 
+//state machine
+always @ ( * )
+begin
+    case (ctrl_state)
+        STATE_RESET: begin
+            issue_ack_out                           <= 1'b0;
+
+            operand_buffer_write_enable             <= 1'b0;
+            baseline_exponent_write_enable          <= 1'b0;
+            fraction_pre_shift_len_write_enable     <= 1'b0;
+
+            rounded_product_buffer_write_enable     <= 1'b0;
+
+            sign_out_write_enable                   <= 1'b0;
+            exponent_out_write_enable               <= 1'b0;
+            fraction_out_write_enable               <= 1'b0;
+            clear_output_enable                     <= 1'b0;
+        end
+
+        STATE_PRE_SHIFT: begin
+            operand_buffer_write_enable             <= 1'b1;
+            baseline_exponent_write_enable          <= 1'b1;
+            fraction_pre_shift_len_write_enable     <= 1'b1;
+
+            rounded_product_buffer_write_enable     <= 1'b0;
+
+            sign_out_write_enable                   <= 1'b0;
+            exponent_out_write_enable               <= 1'b0;
+            fraction_out_write_enable               <= 1'b0;
+            clear_output_enable                     <= 1'b0;
+        end
+
+        STATE_COMPUTE: begin
+            operand_buffer_write_enable             <= 1'b0;
+            baseline_exponent_write_enable          <= 1'b0;
+            fraction_pre_shift_len_write_enable     <= 1'b0;
+
+            rounded_product_buffer_write_enable     <= 1'b1;
+
+            sign_out_write_enable                   <= 1'b0;
+            exponent_out_write_enable               <= 1'b0;
+            fraction_out_write_enable               <= 1'b0;
+            clear_output_enable                     <= 1'b0;
+        end
+
+        STATE_POST_SHIFT: begin
+            operand_buffer_write_enable             <= 1'b0;
+            baseline_exponent_write_enable          <= 1'b0;
+            fraction_pre_shift_len_write_enable     <= 1'b0;
+
+            rounded_product_buffer_write_enable     <= 1'b0;
+
+            sign_out_write_enable                   <= 1'b1;
+            exponent_out_write_enable               <= 1'b1;
+            fraction_out_write_enable               <= 1'b1;
+            clear_output_enable                     <= 1'b1;
+        end
+
+        default: begin
+            operand_buffer_write_enable             <= 1'b0;
+            baseline_exponent_write_enable          <= 1'b0;
+            fraction_pre_shift_len_write_enable     <= 1'b0;
+
+            rounded_product_buffer_write_enable     <= 1'b0;
+
+            sign_out_write_enable                   <= 1'b0;
+            exponent_out_write_enable               <= 1'b0;
+            fraction_out_write_enable               <= 1'b0;
+            clear_output_enable                     <= 1'b0;
+        end
+    endcase
+end
 
 number_round
 #(
-    parameter INPUT_WIDTH_IN_BITS = 32,
-    parameter OUTPUT_WIDTH_IN_BITS = 16, //output width should be smaller than input width
-    parameter ROUND_TYPE = "CHOP"
+    .INPUT_WIDTH_IN_BITS(),
+    .OUTPUT_WIDTH_IN_BITS(), //output width should be smaller than input width
+    .ROUND_TYPE()
 )
 number_round
 (
-    input [(INPUT_WIDTH_IN_BITS - 1):0] original_data_in;
-
-    output reg is_rounded;
-    output reg [(OUTPUT_WIDTH_IN_BITS - 1):0] rounded_data_out;
+    .original_data_in(),
+    .rounded_data_out();
 );
 
 endmodule
-
-				)
-	)`
