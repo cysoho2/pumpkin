46f11cba1dfc93237986f3ff02bc1d7a6fec3706
cysoho2 <406641909@qq.com>
2018-11-14 14:56:08 +0800

    fix code

--- rtl/common/basic_logic/integer_divider.v
+++ rtl/common/basic_logic/integer_divider.v
@@ -20,20 +20,26 @@ module integer_divider
     output reg [OPERAND_WIDTH_IN_BITS - 1 : 0]                                              remainder_out,
 
     output reg                                                                              quotient_sign_out,
-    output reg [OPERAND_WIDTH_IN_BITS - 1 : 0]                                              quotient_out
+    output reg [OPERAND_WIDTH_IN_BITS - 1 : 0]                                              quotient_out,
 
+    output reg                                                                              divide_by_zero
 );
 
 parameter [31:0] stage_0                                                                    = 2'b00; //shift
 parameter [31:0] stage_1                                                                    = 2'b01; //subtract the Divisor register from Remainder register
 parameter [31:0] stage_2                                                                    = 2'b10; //test Remainder, write
 
+parameter STATE_RESET = 2'b00;
+parameter STATE_SHIFT = 2'b01;
+parameter STATE_SUB = 2'b10;
 
 wire [OPERAND_WIDTH_IN_BITS - 1 : 0]                                                        data_from_remainder_left_reg;
 wire                                                                                        is_negative_to_control;
 
-reg [1:0]                                                                                   stage_ctr;
+reg [1:0]                                                                                   state_ctr;
 reg [31:0]                                                                                  remainder_reg_shift_ctr;
+
+reg                                                                                         wait_to_busy_flag;
 reg                                                                                         division_is_finished_flag;
 reg                                                                                         output_clear_flag;
 
@@ -42,11 +48,11 @@ reg                                                                             
 reg [OPERAND_WIDTH_IN_BITS - 1 : 0]                                                         divisor_reg;
 reg [OPERAND_WIDTH_IN_BITS * 2 : 0]                                                         remainder_reg;
 
-reg [OPERAND_WIDTH_IN_BITS : 0]                                                             subtract_result_to_remainder_reg;
-
+wire [OPERAND_WIDTH_IN_BITS : 0]                                                             subtract_result_to_remainder;
 
 assign data_from_remainder_left_reg                                                         = remainder_reg[OPERAND_WIDTH_IN_BITS * 2 - 1 : OPERAND_WIDTH_IN_BITS];
-assign is_negative_to_control                                                               = ~subtract_result_to_remainder_reg[OPERAND_WIDTH_IN_BITS];
+assign subtract_result_to_remainder                                                         = {1'b0, data_from_remainder_left_reg} - {1'b0, divisor_reg};
+assign is_negative_to_control                                                               = subtract_result_to_remainder[OPERAND_WIDTH_IN_BITS];
 
 //idle to busy & busy to idle
 always@(posedge clk_in)
@@ -54,8 +60,9 @@ begin
     if (reset_in)
     begin
         is_valid_out                                                                        <= 1'b0;
-        is_ready_out                                                                        <= 1'b1;
+        // is_ready_out                                                                        <= 1'b1;
 
+        wait_to_busy_flag                                                                   <= 1'b1;
         division_is_finished_flag                                                           <= 1'b0;
         output_clear_flag                                                                   <= 1'b0;
 
@@ -95,14 +102,16 @@ begin
         //idle to busy
         if (is_valid_in)
         begin
-            is_ready_out                                                                    <= 1'b0;
+            if (wait_to_busy_flag)
+            begin
+                is_ready_out                                                                    <= 1'b0;
 
-            divisor_sign_reg                                                                <= divisor_sign_in;
-            dividend_sign_reg                                                               <= dividend_sign_in;
+                divisor_sign_reg                                                                <= divisor_sign_in;
+                dividend_sign_reg                                                               <= dividend_sign_in;
 
-            divisor_reg                                                                     <= divisor_in;
-            remainder_reg                                                                   <= {{1'b0}, {(OPERAND_WIDTH_IN_BITS){1'b0}}, dividend_in};
-
+                divisor_reg                                                                     <= divisor_in;
+                remainder_reg                                                                   <= {{1'b0}, {(OPERAND_WIDTH_IN_BITS){1'b0}}, dividend_in};
+            end
         end
 
         //busy to idle
@@ -132,6 +141,7 @@ begin
 end
 
 //control
+/*
 always@(posedge clk_in)
 begin
     if (reset_in)
@@ -142,19 +152,19 @@ begin
     begin
         if (~ is_ready_out)
         begin
-            case (stage_ctr)
+            case (state_ctr)
             stage_0:begin
                 //shift
                 remainder_reg                                                               <= remainder_reg << 1;
             end
             stage_1:begin
-                subtract_result_to_remainder_reg                                            <= {1'b1, data_from_remainder_left_reg} - {1'b0, divisor_reg};
+                subtract_result_to_remainder                                                <= {1'b1, data_from_remainder_left_reg} - {1'b0, divisor_reg};
             end
             stage_2:begin
                 //write
                 if (~ is_negative_to_control)
                 begin
-                    remainder_reg[OPERAND_WIDTH_IN_BITS * 2 - 1 : OPERAND_WIDTH_IN_BITS]    <= subtract_result_to_remainder_reg;
+                    remainder_reg[OPERAND_WIDTH_IN_BITS * 2 - 1 : OPERAND_WIDTH_IN_BITS]    <= subtract_result_to_remainder;
                 end
                 //update
                 remainder_reg[0]                                                            <= is_negative_to_control? 1'b0 : 1'b1;
@@ -164,26 +174,74 @@ begin
         end
     end
 end
+*/
+
+always @ ( * )
+begin
+    case (state_ctr)
+        STATE_SHIFT: begin
+            remainder_reg                                                               <= remainder_reg << 1;
+            remainder_reg_shift_ctr                                                     <= remainder_reg_shift_ctr + 1'b1;
+        end
+
+        STATE_SUB: begin
+            //write
+            if (~ is_negative_to_control)
+            begin
+                remainder_reg[OPERAND_WIDTH_IN_BITS * 2 - 1 : OPERAND_WIDTH_IN_BITS]    <= subtract_result_to_remainder;
+            end
+            //update
+            remainder_reg[0]                                                            <= is_negative_to_control? 1'b0 : 1'b1;
+        end
+
+        default: ;
+    endcase
+end
 
 //stage counter
+/*
 always@(posedge clk_in)
 begin
     if (reset_in | is_ready_out)
     begin
-        stage_ctr                                                                           <= 1'b0;
+        state_ctr                                                                           <= 1'b0;
     end
     else
     begin
-        if (stage_ctr == 2'b10)
+        if (state_ctr == 2'b10)
         begin
-            stage_ctr                                                                       <= 2'b00;
+            state_ctr                                                                       <= 2'b00;
         end
         else
         begin
-            stage_ctr                                                                       <= stage_ctr + 1'b1;
+            state_ctr                                                                       <= state_ctr + 1'b1;
         end
     end
 end
+*/
 
+always @(posedge clk_in)
+begin
+    if (reset_in)
+    begin
+        state_ctr <= STATE_RESET;
+    end
+    else
+    begin
+        case (state_ctr)
+            STATE_RESET: begin
+                state_ctr <= STATE_SHIFT;
+            end
+
+            STATE_SHIFT: begin
+                state_ctr <= STATE_SUB;
+            end
+
+            STATE_SUB: begin
+                state_ctr <= STATE_SHIFT;
+            end
+        endcase
+    end
+end
 
 endmodule
